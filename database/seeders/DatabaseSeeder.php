cf6f9265d59855760182b19d2e478a1821525fed
soudshoja <soud@alphia.net>
2026-02-26 13:20:08 +0000

    chore: update database seeder to skip subscriptions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            SubscriptionPlanSeeder::class,
             NotificationTemplateSeeder::class,
         ]);
     }
