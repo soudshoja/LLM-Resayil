0560e7256076215f0b0808e7a1b524eb7a51ee6d
LLM Resayil Admin <admin@llm.resayil.io>
2026-02-28 23:06:11 +0800

    fix: update public/index.php paths for production deployment

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../llm.resayil.io/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/../llm.resayil.io/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
