513d62e87a829b127ad4a96092690faac966665a
LLM Resayil Admin <admin@llm.resayil.io>
2026-02-28 16:05:04 +0800

    Fix DatabaseSeeder order: UserSeeder before SubscriptionPlanSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             NotificationTemplateSeeder::class,
-            SubscriptionPlanSeeder::class,
             UserSeeder::class,
+            SubscriptionPlanSeeder::class,
         ]);
     }
 }
